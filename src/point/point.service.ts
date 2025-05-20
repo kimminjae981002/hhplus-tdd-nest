a317ec4e785d13a33864bfe048f88dca07225dd3
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2025-05-20 22:27:10 +0900

    [FEAT] Point Testing 1. point 조회 test

--- src/point/point.service.ts
+++ src/point/point.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PointHistoryTable } from 'src/database/pointhistory.table';
+import { UserPointTable } from 'src/database/userpoint.table';
 
 @Injectable()
-export class PointService {}
+export class PointService {
+  constructor(
+    private readonly userDb: UserPointTable,
+    private readonly historyDb: PointHistoryTable,
+  ) {}
+
+  async getPointOfUser(userId: number) {
+    try {
+      // 유저가 존재하는지 체크
+      const user = await this.userDb.selectById(userId);
+
+      if (user === null || !user) {
+        throw new BadRequestException('유저가 존재하지 않습니다.');
+      }
+
+      // 유저가 있다면 포인트 반환
+      return {
+        result: 'success',
+        message: { userId: user.id, point: user.point },
+      };
+    } catch (error) {
+      if (error instanceof BadRequestException) {
+        throw error; // 원래 에러는 그대로 던진다
+      }
+      console.error('error: ', error);
+      throw new BadRequestException('포인트 조회 중 에러가 발생했습니다.');
+    }
+  }
+}
